9b02fb97a7564f85ff8891cf2ddc8bfacc91148b
dudw <dudwbtc@gmail.com>
2016-05-19 16:59:14 +0800

    init project

---
+++ Chapter01-GettingStartedwithViewAnimations/Bahama02/Bahama02/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bahama02
+//
+//  Created by dudawei on 16/5/19.
+//  Copyright © 2016年 winchannel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
