e7244b9cd3c6922216992708c4a935f7fdff83f8
dudw <dudw_btc@163.com>
2016-05-23 00:34:19 +0800

    ch4

---
+++ Chapter04-ViewAnimationInPractice/Flight Info/Flight Info/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flight Info
+//
+//  Created by dudw on 16/5/22.
+//  Copyright © 2016年 dudw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
