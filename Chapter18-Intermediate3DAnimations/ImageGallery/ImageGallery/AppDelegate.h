d1dc80c29f5d57289481e99a0db014661effd2d2
dudw <dudw_btc@163.com>
2016-06-29 21:06:50 +0800

    init intermediate

---
+++ Chapter18-Intermediate3DAnimations/ImageGallery/ImageGallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageGallery
+//
+//  Created by dudw on 16/6/29.
+//  Copyright © 2016年 dudw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
